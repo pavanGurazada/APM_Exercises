97ca8214122b7032c71828de2219ea5f77c6e91c
pavanGurazada <pavankumar.gurazada@gmail.com>
2018-01-30 13:18:23 +0400

    Added code for exercises

--- Ch4ModelTuning.R
+++ Ch4ModelTuning.R
@@ -87,6 +87,9 @@ glimpse(cvSplits)
 
 # Once data has been split, we can fit models and use this to make predictions
 
+# In general, ask yourself two questions. Are there any hyperparametes of the model
+# that need to be estimated? How should the cross validation be done?
+
 # We fit a k-nearest neighbors classification using a single resample split
 # Then we use the fit to predict on the test set
 
@@ -180,9 +183,205 @@ svmFit <- train(Class ~ .,
                 trControl = trainControl(method = "repeatedcv",
                                          number = 10,
                                          repeats = 5),
-                tuneGrid = expand.grid(C = seq(0.25, 128, length.out = 25),
-                                       sigma = seq(0.01, 0.04, length.out = 25)))
+                tuneGrid = expand.grid(C = seq(0.25, 128, length.out = 5),
+                                       sigma = seq(0.01, 0.04, length.out = 5)))
 
 svmFit$results
 plot(svmFit, scales = list(x = list(log = 2)))
 print(svmFit)
+
+# GERMAN CREDIT LOGISTIC FIT
+
+logisticFit <- train(Class ~ .,
+                     data = germanCreditTrain,
+                     method = "glm",
+                     trControl = trainControl(method = "repeatedcv",
+                                              number = 10,
+                                              repeats = 5))
+
+summary(logisticFit)
+
+# There seem to be some offending columns that are prompting warnings. Lets
+# try excluding them
+
+logisticFit <- train(Class ~ .,
+                     data = germanCreditTrain %>% select(-CheckingAccountStatus.none, 
+                                                         -EmploymentDuration.Unemployed, 
+                                                         -Personal.Male.Married.Widowed),
+                     method = "glm",
+                     trControl = trainControl(method = "repeatedcv",
+                                              number = 10,
+                                              repeats = 5))
+logisticFit$results
+
+# Now comparing models
+
+resamp <- resamples(list(SVM = svmFit, Logistic = logisticFit))
+summary(resamp)
+
+# No statistical test required to see that the logsitic regression is as good
+# as a more complex support vector machine
+
+# Exercise 4.2
+
+data("permeability")
+glimpse(permeability)
+
+ggplot(as.data.frame(permeability), aes(x = permeability)) +
+  geom_histogram(aes(y = (..count..)/sum(..count..)), binwidth = 5) + # Need to grok this double dots!
+  scale_y_continuous(labels = scales::percent) +
+  labs(x = "Permeability",
+       y = "Percent of total")
+
+# Exercise 4.3
+
+data("ChemicalManufacturingProcess")
+glimpse(ChemicalManufacturingProcess)
+
+# Check for degenerate features
+
+length(nearZeroVar(ChemicalManufacturingProcess)) > 0 
+
+chemManufactProcessData <- ChemicalManufacturingProcess %>% select(-nearZeroVar(ChemicalManufacturingProcess)) 
+
+# Quick check on overall extent of missingness
+
+sum(is.na(chemManufactProcessData))
+
+chemManufactProcessData %>% select_if(function(x) any(is.na(x))) %>% 
+                            summarize_all(funs(sum(is.na(.))*100/length(.)))
+
+# All missing values are concentrated in the manufacturing process features
+# No missing proportions are alarmingly high
+# Lets impute the missing values using k-nearest neighbors; we use 10-fold 
+# crossvalidation with 5 repeats as usual to infer the value of k. We then 
+# apply the imputer to the data and check that there are no more missing 
+# values
+
+naImputer <- preProcess(chemManufactProcessData, 
+                        method = "knnImpute",
+                        trControl = trainControl(method = "repeatedcv",
+                                                 number = 10,
+                                                 repeats = 5), 
+                        tuneGrid = data.frame(k = 2:10))
+
+chemManufactProcessData <- predict(naImputer, chemManufactProcessData)
+if (sum(is.na(chemManufactProcessData)) == 0) {
+  cat("Success! No more missing Values\n")
+}
+
+# Check for highly correlated features 
+
+highCorrVars <- chemManufactProcessData %>% cor() %>% 
+                                            findCorrelation(cutoff = 0.75)
+
+chemManufactProcessData <- chemManufactProcessData %>% select(-highCorrVars)
+glimpse(chemManufactProcessData)
+
+# Plot to see if there is any skew in the predictors
+
+chemManufactProcessData %>% gather("Variable", "Value", 2:37) %>% 
+                            ggplot(aes(x = Value)) +
+                             geom_histogram(binwidth = 5) +
+                             facet_wrap(~Variable)
+
+plsProfileChemFit <- train(Yield ~ .,
+                           data = chemManufactProcessData,
+                           method = "pls",
+                           preProcess = c("center", "scale"),
+                           tuneLength = 10,
+                           trControl = trainControl(method = "repeatedcv",
+                                                    number = 10,
+                                                    repeats = 5))
+R2Values <- plsProfileChemFit$results %>% select(ncomp, Rsquared, RsquaredSD) %>% 
+                                          mutate(RsquaredSE = RsquaredSD/sqrt(length(plsProfileChemFit$control$index)))
+
+ggplot(R2Values, aes(x = ncomp, y = Rsquared)) +
+  geom_point() +
+  geom_errorbar(aes(ymin = Rsquared - RsquaredSE, ymax = Rsquared + RsquaredSE), width = 0.1)
+
+# Now, we fit a succession of models, with little regard for model hyperparameters
+
+plsChemFit <- train(Yield ~ .,
+                    data = chemManufactProcessData,
+                    method = "pls",
+                    preProcess = c("center", "scale"),
+                    tuneLength = 10,
+                    trControl = trainControl(method = "repeatedcv",
+                                             number = 10,
+                                             repeats = 5))
+
+lmChemFit <- train(Yield ~ .,
+                   data = chemManufactProcessData,
+                   method = "lm",
+                   preProcess = c("center", "scale"),
+                   trControl = trainControl(method = "repeatedcv",
+                                            number = 10,
+                                            repeats = 5))
+svmChemFit <- train(Yield ~ .,
+                    data = chemManufactProcessData,
+                    method = "svmLinear",
+                    preProcess = c("center", "scale"),
+                    tuneLength = 10,
+                    trControl = trainControl(method = "repeatedcv",
+                                             number = 10,
+                                             repeats = 5))
+rfChemFit <- train(Yield ~ .,
+                   data = chemManufactProcessData,
+                   method = "rf",
+                   preProcess = c("center", "scale"),
+                   tuneLength = 10,
+                   trControl = trainControl(method = "repeatedcv",
+                                            number = 10,
+                                            repeats = 5))
+# Exercise 4.4
+
+data(oil)
+glimpse(oilType)
+
+# This is a nice puzzle. It illustrates the heavy lifting that is done by 
+# createDataPartition()
+
+oilType %>% table() %>% prop.table()
+sample(oilType) %>% table() %>% prop.table() # full sample
+
+# Create a data partition with 60-40 split, using sample
+# We create 1000 such resamples and check the variability
+
+sampleSize <- floor(0.6*length(oilType))
+numResamples <- 1000
+randDataPartitions <- matrix(nrow = numResamples, ncol = 7)
+colnames(randDataPartitions) <- LETTERS[1:7]
+
+for (sample in 1:numResamples){
+  randDataPartitions[sample, ] <- sample(oilType, size = sampleSize, replace = FALSE) %>% 
+                                  table() %>% 
+                                  prop.table()
+}
+
+
+trainingRows <- createDataPartition(oilType, p = 0.6, times = numResamples)
+
+caretDataPartitions <- matrix(nrow = numResamples, ncol = 7)
+colnames(caretDataPartitions) <- LETTERS[1:7]
+
+for (sample in 1:numResamples) {
+  caretDataPartitions[sample, ] <- oilType[trainingRows[[sample]]] %>% 
+                                   table() %>% 
+                                   prop.table()
+}
+
+# Original Data
+oilType %>% table() %>% prop.table()
+
+# Random sampling
+randDataPartitions %>% as.data.frame() %>% 
+                       summarize_all(funs(mean, sd))
+
+# Stratified sampling using caret
+
+caretDataPartitions %>% as.data.frame() %>% 
+                        summarize_all(funs(mean, sd))
+
+# There is no variability in the data partitions created by caret!
+
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 Replication of text and exercises from the book [Applied Predictive Modeling](http://appliedpredictivemodeling.com) by Kuhn and Johnson (2013)
 
-The code is annotated heavily with my understanding of the text and influenced heavily by tidyverse.
+The code is annotated heavily with my understanding of the text and infused
+with a functional programming workflow. Tidyverse takes center stage. 
